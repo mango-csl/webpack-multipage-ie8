a5a251fa77e16c90b204c24fd452c8ab8d0e4023
csl <453826887@qq.com>
2019-05-05 10:12:25 +0800

    fix：修复spa页面，资源加载的写法

--- src/views/spa/spa.js
+++ src/views/spa/spa.js
@@ -1,6 +1,6 @@
 require('../../assets/styles/base/index.less');
 require('../../utils/layui/layui_depend');
-layui.use('element', function() {
+layui.use('element', function () {
     var element = layui.element; //导航的hover效果、二级菜单等功能，需要依赖element模块
 });
 var Data = {};
@@ -8,7 +8,6 @@ var Data = {};
 // 增加事件
 location.hash = '#1'; //初始地址
 //todo issue：当前页刷新时，无法进入 $(window).on('hashchange', callback) callback方法
-// 市面上应该有基于IE8的路由实现方案
 
 // todo 懒加载的实现,在进入新页签时,才加载当前页签所需资源.
 // 思路:webpack require写法的问题? 统一分装到route组件中?
@@ -39,32 +38,33 @@ $(window).on('hashchange', function () {
             });
             break;
         case '3':
-            var html = '<div id="main" style="width: 800px;height: 600px"></div>';
-            var content = $('#content');
-            content.html(html);
-            var echarts = require('echarts/lib/echarts');
-            // 引入柱状图
-            require('echarts/lib/chart/bar');
-            // 引入提示框和标题组件
-            require('echarts/lib/component/tooltip');
-            require('echarts/lib/component/title');
-            // 基于准备好的dom，初始化echarts实例
-            var myChart = echarts.init(document.getElementById('main'));
-            // 绘制图表
-            myChart.setOption({
-                title: {
-                    text: 'ECharts 入门示例'
-                },
-                tooltip: {},
-                xAxis: {
-                    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-                },
-                yAxis: {},
-                series: [{
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
-                }]
+            require(['echarts/lib/echarts'], function (echarts) {
+                var html = '<div id="main" style="width: 800px;height: 600px"></div>';
+                var content = $('#content');
+                content.html(html);
+                // 引入柱状图
+                require('echarts/lib/chart/bar');
+                // 引入提示框和标题组件
+                require('echarts/lib/component/tooltip');
+                require('echarts/lib/component/title');
+                // 基于准备好的dom，初始化echarts实例
+                var myChart = echarts.init(document.getElementById('main'));
+                // 绘制图表
+                myChart.setOption({
+                    title: {
+                        text: 'ECharts 入门示例'
+                    },
+                    tooltip: {},
+                    xAxis: {
+                        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+                    },
+                    yAxis: {},
+                    series: [{
+                        name: '销量',
+                        type: 'bar',
+                        data: [5, 20, 36, 10, 10, 20]
+                    }]
+                });
             });
             break;
         case '4':
